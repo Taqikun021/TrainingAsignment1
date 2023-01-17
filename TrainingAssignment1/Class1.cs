826440e8ed72eee1b473225228cd133ab0cb8852
kuntaqi <ahmad.taqiyudin@formulatrix.com>
2023-01-17 11:02:28 +0700

    Initial commit

---
+++ TrainingAssignment1/Class1.cs
@@ -0,0 +1,32 @@
+﻿namespace TrainingAsignment1;
+
+public class Class1
+{
+    static void Foobar(int number)
+    {
+        for (int i = 1; i <= number; i++)
+        {
+            if (i % 3 ==0 && i % 5 == 0)
+            {
+                Console.Write("foobar ");
+            }
+            else if (i % 5 == 0)
+            {
+                Console.Write("bar ");
+            }
+            else if (i % 3 == 0)
+            {
+                Console.Write("foo ");
+            }
+            else
+            {
+                Console.Write(i + " ");
+            }
+        }
+    }
+    
+    public static void Main()
+    {
+        Foobar(15);
+    }
+}
