0e5a56ef4ad47e33813b585cb264dcb1108559f8
kuntaqi <ahmad.taqiyudin@formulatrix.com>
2023-01-17 12:59:24 +0700

    code style updates

--- TrainingAssignment1/Class1.cs
+++ TrainingAssignment1/Class1.cs
@@ -2,31 +2,19 @@
 
 public class Class1
 {
-    static void Foobar(int number)
+  static void Foobar( int number )
+  {
+    for( int i = 1; i <= number; i++ )
     {
-        for (int i = 1; i <= number; i++)
-        {
-            if (i % 3 ==0 && i % 5 == 0)
-            {
-                Console.Write("foobar ");
-            }
-            else if (i % 5 == 0)
-            {
-                Console.Write("bar ");
-            }
-            else if (i % 3 == 0)
-            {
-                Console.Write("foo ");
-            }
-            else
-            {
-                Console.Write(i + " ");
-            }
-        }
+      if( i % 3 == 0 && i % 5 == 0 ) Console.Write( "foobar " );
+      else if( i % 5 == 0 ) Console.Write( "bar " );
+      else if( i % 3 == 0 ) Console.Write( "foo " );
+      else Console.Write( i + " " );
     }
-    
-    public static void Main()
-    {
-        Foobar(15);
-    }
+  }
+
+  public static void Main()
+  {
+    Foobar( 15 );
+  }
 }
